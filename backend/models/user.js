2c8859352eb2e8b7a84fcb37691369a304eeea9a
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2023-09-06 23:29:27 +0530

    Updated user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,7 +5,8 @@ const UserSchema = new Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
     firstName: {type: String, required: true},
-    familyName: {type: String, required: true}
+    familyName: {type: String, required: true},
+    isAdmin: {type: Boolean, required: true, default: false}
 });
 
 module.exports = mongoose.model("User", UserSchema);
